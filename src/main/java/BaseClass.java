d6b9912d4f1a558eba30b2f4febcc520ff7b938d
Erik Gustafsson <erikgust2@gmail.com>
2022-04-26 09:54:18 +0200

    Failed Jenkins and Spring preparation sadge

--- src/main/java/BaseClass.java
+++ src/main/java/BaseClass.java
@@ -1,2 +1,14 @@
-public class BaseClass {
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+@RestController
+public class BaseClass extends SpringBootServletInitializer{
+    public static void main(String[] args) {
+        SpringApplication.run(BaseClass.class, args);
+    }
+
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
 }
